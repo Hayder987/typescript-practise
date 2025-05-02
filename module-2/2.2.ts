855bbd4b9121f203e9b05effe9d77de049501009
hayder <hayderbd4290@gmail.com>
2025-05-02 20:45:43 +0600

    interface use on array and function

--- module-2/2.2.ts
+++ module-2/2.2.ts
@@ -20,4 +20,31 @@
         age : 30,
         roll : 21
     }
+
+    type Arr1 = number[];
+
+    const arr1:Arr1 = [1, 2, 1]
+
+    interface Arr2  {
+        [index:number] : string
+    }
+
+    const arr2:Arr2 = ["1", "2", "3"];
+
+    type Add1 = (num1:number, num2:number)=> number
+
+    const add1:Add1 = (num1, num2)=>{
+       return num1 + num2
+    }
+
+    interface Add2 {
+        (num1:number, num2:number) : number
+    }
+
+    const add2:Add2 = (num1, num2)=>{
+        return num1 + num2
+     }
+
+     console.log(add2(2, 4))
+
 }
