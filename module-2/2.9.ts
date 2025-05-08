3d1a6d7624bf1407651000c69135c069d792e26d
hayder <hayderbd4290@gmail.com>
2025-05-08 18:16:49 +0600

    create constrains

---
+++ module-2/2.9.ts
@@ -0,0 +1,31 @@
+
+{
+    // constrains
+
+    const makeUser =<T extends{name:string, age:number}> (user:T)=>{
+        const data = "he is Student"
+      return{
+        ...user,
+        data
+      }
+    }
+
+    const user1 = makeUser({
+        name:"hayder",
+        age:30,
+        isMarried:true
+    });
+
+    const user2 = makeUser({
+        name:"hayder",
+        age:30,
+        good:true
+    });
+    console.log(user1, user2)
+  
+
+
+
+
+
+}
